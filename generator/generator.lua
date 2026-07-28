84be9ba4b51e6a3226256be4ff10bc73c98562f5
Rofli Souza <rofli@kakunyn.com>
2026-07-28 00:00:36 -0300

    Fix MSVC binding generation

--- generator/generator.lua
+++ generator/generator.lua
@@ -67,8 +67,10 @@ local function parseImGuiHeader(header, names, modulename)
 	parser.cimgui_inherited =  dofile([[../../cimgui/generator/output/structs_and_enums.lua]])
 	
 	local include_cmd = COMPILER=="cl" and [[ /I ]] or [[ -I ]]
+	local language_flag = COMPILER=="cl" and [[ /TP ]] or [[ -x c++ ]]
 	local extra_includes = include_cmd.." ../../cimgui "
 	..include_cmd.." ../../cimgui/imgui "
+	..language_flag
 	
 	parser:take_lines(CPRE..extra_includes..header, names, COMPILER)
 	
